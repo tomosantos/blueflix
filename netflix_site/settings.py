7f3e19a6162474073a069806813b7beb107898ce
Wellinton Oliveira <wellintonoliveira04@gmail.com>
2024-02-21 11:16:46 -0300

    update

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -129,7 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_CONTENT_TYPE = 'text/css'
 
 STATICFILES_DIRS =  [
     os.path.join(BASE_DIR, 'static'),
