a30252e44e9b801377df7b02df93f646fab98d12
Wellinton Oliveira <wellintonoliveira04@gmail.com>
2024-02-21 11:06:37 -0300

    update

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -128,16 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-
 STATIC_URL = 'static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
+STATICFILES_DIRS =  [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -146,11 +142,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-CSRF_TRUSTED_ORIGINS = [
-    'https://blueflix-dev-jhtb.3.us-1.fl0.io/',
-    'https://blueflix.onrender.com',
-]
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
